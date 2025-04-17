0533e9a0b127899da89ad6c47fc4720728fccba9
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-17 13:48:10 -0600

    build export-bytecode task and move logic to lib/ directory

---
+++ src/lib/export_bytecode.ts
@@ -0,0 +1,98 @@
+import pkg from '../../package.json';
+import type {
+  BytecodeExporterConfig,
+  BytecodeExporterConfigEntry,
+} from '../types.js';
+import { clearBytecode } from './clear_bytecode.js';
+import { HardhatPluginError } from 'hardhat/plugins';
+import type { HookContext } from 'hardhat/types/hooks';
+import fs from 'node:fs';
+import path from 'node:path';
+
+export const exportBytecode = async (
+  context: HookContext,
+  configEntries: BytecodeExporterConfig,
+) => {
+  await clearBytecode(context, configEntries);
+
+  await Promise.all(
+    configEntries.map((entry) => exportBytecodeGroup(context, entry)),
+  );
+};
+
+const exportBytecodeGroup = async (
+  context: HookContext,
+  config: BytecodeExporterConfigEntry,
+) => {
+  const outputDirectory = path.resolve(context.config.paths.root, config.path);
+
+  // validate that the output directory is not the Hardhat root directory to prevent accidental file deletion
+
+  if (outputDirectory === context.config.paths.root) {
+    throw new HardhatPluginError(
+      pkg.name,
+      'resolved path must not be root directory',
+    );
+  }
+
+  // get list of all contracts and filter according to configuraiton
+
+  const fullNames = Array.from(
+    await context.artifacts.getAllFullyQualifiedNames(),
+  ).filter((fullName) => {
+    if (config.only.length && !config.only.some((m) => fullName.match(m)))
+      return false;
+    if (config.except.length && config.except.some((m) => fullName.match(m)))
+      return false;
+    return true;
+  });
+
+  // get contract artifacts
+
+  const artifacts = await Promise.all(
+    fullNames.map((fullName) => context.artifacts.readArtifact(fullName)),
+  );
+
+  // filter out 0-size contracts and generate export file contents
+
+  const outputData = artifacts
+    .filter(({ bytecode }) => bytecode.length)
+    .map((artifact) => {
+      const { sourceName, contractName, bytecode } = artifact;
+
+      const contents = bytecode.replace(/^0x/, '');
+      const destination =
+        path.resolve(outputDirectory, config.rename(sourceName, contractName)) +
+        '.bin';
+
+      return { destination, contents };
+    });
+
+  // check for filename clashes among exported files
+
+  outputData.reduce(
+    (acc: { [destination: string]: string }, { destination, contents }) => {
+      const previousContents = acc[destination];
+
+      if (previousContents && previousContents !== contents) {
+        throw new HardhatPluginError(
+          pkg.name,
+          `multiple distinct contracts share same output destination: ${destination}`,
+        );
+      }
+
+      acc[destination] = contents;
+      return acc;
+    },
+    {},
+  );
+
+  // write export files to disk
+
+  await Promise.all(
+    outputData.map(async ({ destination, contents }) => {
+      await fs.promises.mkdir(path.dirname(destination), { recursive: true });
+      await fs.promises.writeFile(destination, contents);
+    }),
+  );
+};
--- src/tasks/export_bytecode.ts
+++ src/tasks/export_bytecode.ts
@@ -1,100 +1,13 @@
-import { name as pluginName } from '../../package.json';
-import { BytecodeExporterConfigEntry } from '../index';
 import { TASK_EXPORT_BYTECODE } from '../task_names.js';
-import fs from 'fs';
-import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names';
-import { task, subtask, types } from 'hardhat/config';
-import { HardhatPluginError } from 'hardhat/plugins';
-import path from 'path';
+import { task } from 'hardhat/config';
 
-task(TASK_EXPORT_BYTECODE)
-  .addFlag('noCompile', "Don't compile before running this task")
-  .setAction(async (args, hre) => {
-    if (!args.noCompile) {
-      await hre.run(TASK_COMPILE, { noExportbytecode: true });
-    }
-
-    const configs = hre.config.bytecodeExporter;
-
-    await Promise.all(
-      configs.map((bytecodeGroupConfig) => {
-        return hre.run('export-bytecode-group', { bytecodeGroupConfig });
-      }),
-    );
-  });
-
-subtask('export-bytecode-group')
-  .addParam(
-    'bytecodeGroupConfig',
-    'a single bytecode-exporter config object',
-    undefined,
-    types.any,
+export default task(TASK_EXPORT_BYTECODE)
+  .setDescription(
+    'Extract bytecode from compilation artifacts and write to a directory',
   )
-  .setAction(async (args, hre) => {
-    const { bytecodeGroupConfig: config } = args as {
-      bytecodeGroupConfig: BytecodeExporterConfigEntry;
-    };
-
-    const outputDirectory = path.resolve(hre.config.paths.root, config.path);
-
-    if (outputDirectory === hre.config.paths.root) {
-      throw new HardhatPluginError(
-        pluginName,
-        'resolved path must not be root directory',
-      );
-    }
-
-    const outputData: { bytecode: string; destination: string }[] = [];
-
-    const fullNames = await hre.artifacts.getAllFullyQualifiedNames();
-
-    await Promise.all(
-      fullNames.map(async (fullName) => {
-        if (config.only.length && !config.only.some((m) => fullName.match(m)))
-          return;
-        if (
-          config.except.length &&
-          config.except.some((m) => fullName.match(m))
-        )
-          return;
-
-        let { bytecode, sourceName, contractName } =
-          await hre.artifacts.readArtifact(fullName);
-
-        bytecode = bytecode.replace(/^0x/, '');
-
-        if (!bytecode.length) return;
-
-        const destination =
-          path.resolve(
-            outputDirectory,
-            config.rename(sourceName, contractName),
-          ) + '.bin';
-
-        outputData.push({ bytecode, destination });
-      }),
-    );
-
-    outputData.reduce((acc, { destination }) => {
-      if (acc.has(destination)) {
-        throw new HardhatPluginError(
-          pluginName,
-          `duplicate output destination: ${destination}`,
-        );
-      }
-
-      acc.add(destination);
-      return acc;
-    }, new Set());
-
-    if (config.clear) {
-      await hre.run('clear-bytecode-group', { path: config.path });
-    }
-
-    await Promise.all(
-      outputData.map(async ({ bytecode, destination }) => {
-        await fs.promises.mkdir(path.dirname(destination), { recursive: true });
-        await fs.promises.writeFile(destination, bytecode, { flag: 'w' });
-      }),
-    );
-  });
+  .addFlag({
+    name: 'noCompile',
+    description: "Don't compile before running this task",
+  })
+  .setAction(import.meta.resolve('../actions/export_bytecode.js'))
+  .build();
