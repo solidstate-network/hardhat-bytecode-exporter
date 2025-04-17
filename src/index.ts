9e3a2af3b17c2048b618e02dcd18db0595d92909
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-17 13:48:53 -0600

    register tasks in HardhatPlugin

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import pkg from '../package.json';
-import './tasks/clear_bytecode';
-import './tasks/export_bytecode';
+import taskClearBytecode from './tasks/clear_bytecode.js';
+import taskExportBytecode from './tasks/export_bytecode.js';
 import { globalOption } from 'hardhat/config';
 import { ArgumentType } from 'hardhat/types/arguments';
 import 'hardhat/types/config';
@@ -9,6 +9,7 @@ import type { HardhatPlugin } from 'hardhat/types/plugins';
 const plugin: HardhatPlugin = {
   id: pkg.name!,
   npmPackage: pkg.name!,
+  tasks: [taskClearBytecode, taskExportBytecode],
   hookHandlers: {
     config: import.meta.resolve('./hooks/config.js'),
     solidity: import.meta.resolve('./hooks/solidity.js'),
