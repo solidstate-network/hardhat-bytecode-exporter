b639df84d68eec1426d4863eeeb5eb1ea5555671
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-20 19:46:23 -0600

    skip runOnCompile if coverage global option is set

--- src/hooks/solidity.ts
+++ src/hooks/solidity.ts
@@ -2,8 +2,10 @@ import type { SolidityHooks } from 'hardhat/types/hooks';
 
 export default async (): Promise<Partial<SolidityHooks>> => ({
   onCleanUpArtifacts: async (context, artifactPaths, next) => {
-    // TODO: skip if solidity coverage running
-    if (!context.globalOptions.noExportBytecode) {
+    if (
+      !context.globalOptions.noExportBytecode &&
+      !context.globalOptions.coverage
+    ) {
       const entries = context.config.bytecodeExporter.filter(
         (entry) => entry.runOnCompile,
       );
