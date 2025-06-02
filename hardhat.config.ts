5404a8ca63365ad9f3097b93dbdde09b99f35325
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-02 13:04:52 -0600

    use solidstate contracts package, import contract for compilation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,10 @@ import type { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
   plugins: [HardhatBytecodeExporter],
+  solidity: {
+    version: '0.8.30',
+    dependenciesToCompile: ['@solidstate/contracts/access/ownable/Ownable.sol'],
+  },
 };
 
 export default config;
